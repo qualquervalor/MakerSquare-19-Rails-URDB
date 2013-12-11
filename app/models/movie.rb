f920c57efa2c5dbca01863c7cb6bf67a00e33989
Qualquer Valor <qualquervalor@gmail.com>
2013-12-11 15:05:02 -0600

    created star and roles models. Updated show to loop over roles

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,9 @@
 class Movie < ActiveRecord::Base
   has_many :showtimes
-  
+  has_many :roles
+  has_many :stars, through: :roles
+
+
   def snippet
     self.description.truncate 50
   end
---
+++ app/models/star.rb
@@ -0,0 +1,5 @@
+class Star < ActiveRecord::Base
+  has_many :roles
+  has_many :movies, through: :roles
+
+end
